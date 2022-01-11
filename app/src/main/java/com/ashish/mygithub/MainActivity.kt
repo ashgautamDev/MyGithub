cb7079ad1cb461979a3b720efae28405b68187bf
Ashish gautam <ashish30adev@gmail.com>
2022-01-11 11:40:24 +0530

    Project Setup

---
+++ app/src/main/java/com/ashish/mygithub/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ashish.mygithub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ashish.mygithub.ui.theme.MyGithubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyGithubTheme {
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyGithubTheme {
+    }
+}
